2d99893144df4240b2975506b91253b96691a8ea
Kelsie Porter <krporter22@gmail.com>
2022-12-30 14:57:25 -0700

    Create #employee_names and associated model test

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe Department, type: :model do
   describe 'relationships' do
     it { should have_many :employees }
   end
+
+  describe 'instance methods' do 
+    before :each do 
+      @it = Department.create!(name: 'IT', floor: 'Basement')
+      @accounting = Department.create!(name: 'Accounting', floor: 'First Floor')
+      @marketing = Department.create!(name: 'Marketing', floor: 'Second Floor')
+
+      @employee_1 = Employee.create!(name: 'Christina Aguilera', level: 2, department_id: @it.id)
+      @employee_2 = Employee.create!(name: 'Brittney Spears', level: 2, department_id: @it.id)
+      @employee_3 = Employee.create!(name: 'Justin Timberlake', level: 5, department_id: @accounting.id)
+      @employee_4 = Employee.create!(name: 'Ryan Gosling', level: 5, department_id: @accounting.id)
+      @employee_5 = Employee.create!(name: 'Keri Russell', level: 7, department_id: @marketing.id)
+      @employee_6 = Employee.create!(name: 'JC Chasez', level: 7, department_id: @marketing.id)
+    end
+    describe '#employee_names' do
+      it 'displays the employees name for each department' do 
+
+        expect(@it.employee_names).to eq(['Christina Aguilera', 'Brittney Spears'])
+        expect(@accounting.employee_names).to eq(['Justin Timberlake', 'Ryan Gosling'])
+        expect(@marketing.employee_names).to eq(['Keri Russell', 'JC Chasez'])
+      end
+    end
+  end
 end
