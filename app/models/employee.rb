a9001dcdaf728dca5a04202fcd6c27d2ba3a4533
Kelsie Porter <krporter22@gmail.com>
2022-12-30 18:18:07 -0700

    Create #department_name and associated model test

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,13 @@
 class Employee < ApplicationRecord
   belongs_to :department
-
   has_many :employee_tickets
   has_many :tickets, through: :employee_tickets
+
+  def department_name
+    department.name
+    # For US2
+    # just department returns the whole department object. Calling
+    #.name on department returns the name attribute of the department object.
+    # Need to write a model level test for this method. 
+  end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe Employee, type: :model do
     it { should have_many :employee_tickets }
     it { should have_many(:tickets).through(:employee_tickets) }
   end
+
+  describe 'instance methods' do 
+    before :each do 
+      @it = Department.create!(name: 'IT', floor: 'Basement')
+      @accounting = Department.create!(name: 'Accounting', floor: 'First Floor')
+      @marketing = Department.create!(name: 'Marketing', floor: 'Second Floor')
+  
+      @employee_1 = Employee.create!(name: 'Christina Aguilera', level: 2, department_id: @it.id)
+      @employee_2 = Employee.create!(name: 'Brittney Spears', level: 2, department_id: @it.id)
+      @employee_3 = Employee.create!(name: 'Justin Timberlake', level: 5, department_id: @accounting.id)
+      @employee_4 = Employee.create!(name: 'Ryan Gosling', level: 5, department_id: @accounting.id)
+      @employee_5 = Employee.create!(name: 'Keri Russell', level: 7, department_id: @marketing.id)
+      @employee_6 = Employee.create!(name: 'JC Chasez', level: 7, department_id: @marketing.id)
+    end
+
+    describe '#department_name' do 
+      it 'returns the department name for the employee' do 
+
+        expect(@employee_1.department_name).to eq('IT')
+        expect(@employee_2.department_name).to eq('IT')
+        expect(@employee_3.department_name).to eq('Accounting')
+        expect(@employee_5.department_name).to eq('Marketing')
+        expect(@employee_5.department_name).to_not eq('Accounting')
+      end
+    end
+  end
 end
